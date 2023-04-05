a7fb33651426d256dc39b897eab4c35ea204f737
pokizu1 <pokizugamer@gmail.com>
2023-04-05 17:04:39 +0400

    ok

---
+++ src/app2.js
@@ -0,0 +1,62 @@
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-border-red", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-border-red";
+}
+
+var slideIndex = 1;
+showDiv(slideIndex);
+
+function plusDiv(n) {
+  showDiv(slideIndex += n);
+}
+
+function currentDivs(n) {
+  showDiv(slideIndex = n);
+}
+
+
+function showDiv(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlide");
+  var dots = document.getElementsByClassName("doms");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-border-red", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-border-red";
+}
